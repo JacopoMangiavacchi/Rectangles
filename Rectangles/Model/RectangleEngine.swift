f4d6cc9915a4a643fd5a4727cdce367fe8135826
Jacopo <jmangia@me.com>
2018-05-26 09:41:31 -0700

    no message

--- Rectangles/Model/RectangleEngine.swift
+++ Rectangles/Model/RectangleEngine.swift
@@ -11,7 +11,13 @@ import Foundation
 //Engine Data
 struct RectangleEngine : Engine {
     private var rectangleArray = [Rectangle]()
-    private var intersectionArray: [Rectangle]?
+    private var intersectionArray: [Rectangle]? {
+        didSet {
+            DispatchQueue.main.async { [self] in
+                self.delegate?.refreshRender()
+            }
+        }
+    }
     
     var delegate: EngineEvents?
 
@@ -87,19 +93,16 @@ extension RectangleEngine {
     mutating func reset() {
         rectangleArray.removeAll()
         intersectionArray = nil
-        _fireAsyncRefreshRender()
     }
     
     mutating func addRectangle(_ rect: Rectangle) {
         rectangleArray.append(rect)
         _checkIntersections()
-        _fireAsyncRefreshRender()
     }
 
     mutating func resizeRectangle(pos: Int, to: Rectangle) {
         rectangleArray[pos] = to
         _checkIntersections()
-        _fireAsyncRefreshRender()
     }
 }
 
@@ -113,15 +116,6 @@ extension RectangleEngine {
 //Engine Events
 protocol EngineEvents {
     func refreshRender()
-}
-
-//Engine Events Async Fires
-extension RectangleEngine {
-    private func _fireAsyncRefreshRender() {
-        DispatchQueue.main.async { [self] in
-            self.delegate?.refreshRender()
-        }
-    }
 }
 
 
